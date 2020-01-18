d2a0301c0a37e962b94429c1e626bf7ca2ab4d47
henrytien <tianhangyu8886@gmail.com>
2020-01-18 15:13:26 +0800

    TcpServer

--- src/net/TcpServer.cpp
+++ src/net/TcpServer.cpp
@@ -3,3 +3,28 @@
 
 //
 
+#include "Log4Func.h"
+#include "../../include/net/TcpServer.h"
+
+namespace Henry {
+    int createSocketFd() {
+        int fd = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (fd == Error) {
+            LogError("create socket fd error!");
+            exit(EXIT_FAILURE);
+        }
+        LogInfo("new fd: " + fd.tostring());
+        return fd;
+    }
+
+    TcpServer::TcpServer(const string &ip, unsigned short port) :
+            addr(ip, port), sockfd(createSocketFd()), poller(sockfd.fd()) {
+        sockfd.setTcpNoDelay(false);
+        sockfd.setReusePort(true);
+        sockfd.setKeepAlive(false);
+        sockfd.setReuseAddr(true);
+        sockfd.bindAddress(addr);
+        sockfd.listen();
+    }
+
+}
